a8b7a5153650f66a7c1acd0e000a2774e7df4ad5
allexandresampaio <allexandresss@gmail.com>
2025-05-28 20:06:01 -0300

    primeiros comandos de modularizacao e NPM

---
+++ conceitos/modularizacao-export.js
@@ -0,0 +1,7 @@
+function exibirNomePet(nome){
+    console.log(`O nome do Pet é ${nome}`)
+}
+
+export{//exportando a funcao criada aqui pra poder importar e usar em outros locais do projeto
+    exibirNomePet
+}
